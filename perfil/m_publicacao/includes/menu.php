a1a152d96a585f465d9a94dc856b1886fa711edf
Marcio Yonamine <marcioyonamine@gmail.com>
2016-03-21 17:07:01 -0300

    Publicacao v0.1

---
+++ perfil/m_publicacao/includes/menu.php
@@ -0,0 +1,29 @@
+<?php
+//geram o insert pro framework da igsis
+$pasta = "?perfil=publicacao&p=";
+ ?>
+
+
+<div class="menu-area">
+  <div id="dl-menu" class="dl-menuwrapper">
+	<button class="dl-trigger">Open Menu</button>
+	<ul class="dl-menu">
+		<li><a href="#">Pessoa Fisica</a>
+			<ul class="dl-submenu">
+                <li><a href="<?php echo $pasta ?>frm_lista_publicacaopf">Cadastrar</a></li>
+				<li><a href="<?php echo $pasta ?>frm_listaedita_publicacaopf">Listar Gerados</a></li> 
+            </ul>
+		</li>
+		<li><a href="#">Pessoa Juridica</a>
+			<ul class="dl-submenu">
+                <li><a href="<?php echo $pasta ?>frm_lista_publicacaopj">Cadastrar</a></li>
+				<li><a href="<?php echo $pasta ?>frm_listaedita_publicacaopj">Listar Gerados</a></li> 
+            </ul>
+		</li>
+		<li style="color:white;">-------------------------</li>		
+		<li><a href="index.php?secao=perfil">Carregar módulos</a></li>
+		<li><a href="#">Ajuda</a></li>
+		<li><a href="../index.php">Sair</a></li>
+			</ul>
+  </div>
+</div>	
